7e42928984c6065382c5be9ba52d66f3dec7221f
umairkhan14 <umairkhan14@outlook.com>
2017-09-18 02:08:54 +0500

    static templates added to templates view

--- app/app.js
+++ app/app.js
@@ -3,8 +3,6 @@
 // Declare app level module which depends on views, and components
 angular.module('templateStore', [
   'ngRoute',
-  'templateStore.view1',
-  'templateStore.view2',
   'templateStore.templates'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
